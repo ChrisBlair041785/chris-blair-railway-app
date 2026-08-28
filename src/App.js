2608b2fc23f1b02df119b0b6bfc4497f9377eedc
Chris <chris.blair041785@gmail.com>
2026-08-28 10:59:38 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Chris Blair - Railway App</p>
+        <p>Railway Deployment</p>
       </header>
     </div>
   );
